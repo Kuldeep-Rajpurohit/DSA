e59906dd9c5226046628737ec3fab987b60fa0fc
Kuldeep Rajpurohit <51077392+Kuldeep-Rajpurohit@users.noreply.github.com>
2023-08-20 16:01:14 +0530

    Create 242.Valid_Anagram.py

---
+++ Strings/242.Valid_Anagram/242.Valid_Anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        count = {}
+        
+        for i in s:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        
+        for i in t:
+            if i in count:
+                count[i] -= 1
+            else:
+                return False
+        
+        for i in count:
+            if count[i] != 0:
+                return False
+        
+        return True
