79b266261e0c9e25de2e7a65ee2ff486123765b5
Kuldeep Rajpurohit <51077392+Kuldeep-Rajpurohit@users.noreply.github.com>
2023-08-26 18:54:18 +0530

    Create 852.Peak_Index_in_a_Mountain_Array.py

---
+++ Binary_Search/852.Peak_Index_in_a_Mountain_Array/852.Peak_Index_in_a_Mountain_Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right:
+            mid = left + (right-left)//2
+            if arr[mid-1] < arr[mid] and arr[mid] > arr[mid+1] :
+                return(mid)
+            elif arr[mid-1] > arr[mid]:
+                right = mid
+            else:
+                left = mid
+        return(-1)
